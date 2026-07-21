611a1b8f804755bf3cf56f393371a2c10ba91828
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-07-21 09:18:08 -0300

    Fazendo a primeira % de variacao

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ import yfinance as yf
 def main():
 
     dados = yf.download("PETR4.SA")
+    
+    trades = 0
+    lucro_total = 0
+    print(dados)
 
     for i in range(1, len(dados)):
 
-        preco_hoje = dados["Close"].iloc[i]
-        preco_ontem = dados["Close"].iloc[i - 1]
+        preco_hoje = dados["Close"]["PETR4.SA"].iloc[i]
+        preco_ontem = dados["Close"]["PETR4.SA"].iloc[i - 1]
+        variacao = (preco_hoje - preco_ontem) / preco_ontem
+        
+        
+        if variacao <= -0.02:
+            print("Compra!")
+            print(f"Hoje: {preco_hoje}")
+            print(f"Ontem: {preco_ontem}")
+            print(f"Variacao: {variacao * 100}")
+            print("-------")
 
-        print(preco_hoje)
-        print(preco_ontem)
-        print()
 
 
 
