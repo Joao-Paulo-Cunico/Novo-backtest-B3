a72a5253296e18a13b021a7aaf965ad50d239a92
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-07-22 09:52:50 -0300

    Corrigindo problemas futuros com droplevel

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ def main():
 
     dados = yf.download("PETR4.SA")
     
+    dados = dados.droplevel("Ticker", axis=1)
+    
     trades = 0
     lucro_total = 0
-    print(dados)
 
     for i in range(1, len(dados)):
 
-        preco_hoje = dados["Close"]["PETR4.SA"].iloc[i]
-        preco_ontem = dados["Close"]["PETR4.SA"].iloc[i - 1]
+        preco_hoje = dados["Close"].iloc[i]
+        preco_ontem = dados["Close"].iloc[i - 1]
         variacao = (preco_hoje - preco_ontem) / preco_ontem
         
         
