2d5e22da8da6b840f285d9e634d2e2f10e70b1c3
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-07-26 21:22:05 -0300

    inplementado o preco de compra, lucro e total de trades que ocorreu

--- main.py
+++ main.py
@@ -8,21 +8,33 @@ def main():
     
     trades = 0
     lucro_total = 0
+    testevariacao = 0.01
 
     for i in range(1, len(dados)):
 
         preco_hoje = dados["Close"].iloc[i]
         preco_ontem = dados["Close"].iloc[i - 1]
+        
         variacao = (preco_hoje - preco_ontem) / preco_ontem
+        valor_compra = preco_ontem * (1 - testevariacao)
+        lucro = preco_hoje - valor_compra
         
         
-        if variacao <= -0.02:
-            print("Compra!")
-            print(f"Hoje: {preco_hoje}")
-            print(f"Ontem: {preco_ontem}")
-            print(f"Variacao: {variacao * 100}")
-            print("-------")
+        if variacao <= testevariacao:
+            trades += 1
 
+            print("Compra!")
+            
+            print(f"Hoje: {preco_hoje:.2f}")
+            print(f"Ontem: {preco_ontem:.2f}")
+            print(f"Variacao: {variacao * 100:.2f}%\n" )
+
+            print(f"Preco de compra: {valor_compra:.3f}")
+            print(f"O lucro foi: {lucro:.2f}")
+
+            print(f"{trades}")
+
+            print("-------\n")
 
 
 
