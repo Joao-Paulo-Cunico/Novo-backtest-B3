3625328e2ddac5050ada1229c31e36e9daef461b
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-08-26 09:14:11 -0300

    Adicionando a parte de maior lucro e maior perda

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def executar_backtest(dados):
     print("Executando backtest...")
     lucro_total = 0
     trades_vencedores = 0
-    #estatistica para operacao
+    # estatistica para operacao
     queda_para_compra = 0.01
 
     for i in range(1, len(dados)):
@@ -47,30 +47,43 @@ def calcular_estatisticas(trades_realizados):
     total_trades = len(trades_realizados)
     trades_vencedores = 0
     lucro_total = 0
-
+    maior_lucro = None
+    maior_prejuizo = None
 
     for trade in trades_realizados:
         lucro_total += trade["lucro"]
+        lucro = trade["lucro"]
+
+        if maior_lucro is None or lucro > maior_lucro:
+            maior_lucro = lucro
+
+        if maior_prejuizo is None or lucro < maior_prejuizo:
+            maior_prejuizo = lucro
 
         if trade["lucro"] > 0:
             trades_vencedores += 1
 
     trades_perdedores = total_trades - trades_vencedores
-        
+
     if total_trades > 0:
         taxa_acerto = (trades_vencedores / total_trades) * 100
         lucro_medio = lucro_total / total_trades
     else:
         taxa_acerto = 0
         lucro_medio = 0
-               
+        maior_lucro = 0
+        maior_prejuizo = 0
+
     return (
-    total_trades,
-    trades_vencedores,
-    trades_perdedores,
-    lucro_total,
-    lucro_medio,
-    taxa_acerto)
+        total_trades,
+        trades_vencedores,
+        trades_perdedores,
+        lucro_total,
+        lucro_medio,
+        taxa_acerto,
+        maior_lucro,
+        maior_prejuizo,
+    )
 
 
 def relatorio(
@@ -79,7 +92,9 @@ def relatorio(
     trades_perdedores,
     lucro_total,
     lucro_medio,
-    taxa_acerto
+    taxa_acerto,
+    maior_lucro,
+    maior_prejuizo
 ):
     print("========== RELATÓRIO ==========")
     print(f"Total de trades: {total_trades}")
@@ -88,22 +103,35 @@ def relatorio(
     print(f"Lucro total: {lucro_total:.2f}")
     print(f"Lucro médio: {lucro_medio:.2f}")
     print(f"Taxa de acerto: {taxa_acerto:.2f}%")
+    print(f"Maior lucro: {maior_lucro:.2f}")
+    print(f"Maior prejuízo: {maior_prejuizo:.2f}")
 
 
 def main():
     dados = baixar_dados("PETR4.SA")
 
     trades_realizados, lucro_total = executar_backtest(dados)
-    
-    total_trades, trades_vencedores, trades_perdedores, lucro_total, lucro_medio, taxa_acerto = calcular_estatisticas(trades_realizados)
-    
+
+    (
+        total_trades,
+        trades_vencedores,
+        trades_perdedores,
+        lucro_total,
+        lucro_medio,
+        taxa_acerto,
+        maior_lucro,
+        maior_prejuizo
+    ) = calcular_estatisticas(trades_realizados)
+
     relatorio(
         total_trades,
         trades_vencedores,
         trades_perdedores,
         lucro_total,
         lucro_medio,
-        taxa_acerto
+        taxa_acerto,
+        maior_lucro,
+        maior_prejuizo
     )
 
 
